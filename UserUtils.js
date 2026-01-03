f2d7b9658fcee2ecac26ee308c2a3bbcd47d3ae5
Wilfried Waret <wilfried.waret@gmail.com>
2026-01-02 23:08:46 -1000

    Add UserUtils script include

---
+++ UserUtils.js
@@ -0,0 +1,40 @@
+/**
+ * Script Include: UserUtils
+ * Description: Utility class for user-related operations
+ * API Name: UserUtils
+ * Client Callable: false
+ * Accessible from: All application scopes
+ */
+var UserUtils = Class.create();
+UserUtils.prototype = {
+    initialize: function() {
+    },
+
+    /**
+     * Get the manager of a user
+     * @param {string} userId - The sys_id of the user
+     * @returns {GlideRecord|null} - The manager's GlideRecord or null if not found
+     */
+    getManager: function(userId) {
+        if (!userId) {
+            return null;
+        }
+
+        var userGR = new GlideRecord('sys_user');
+        if (userGR.get(userId)) {
+            var managerSysId = userGR.getValue('manager');
+            
+            if (managerSysId) {
+                var managerGR = new GlideRecord('sys_user');
+                if (managerGR.get(managerSysId)) {
+                    return managerGR;
+                }
+            }
+        }
+        
+        return null;
+    },
+
+    type: 'UserUtils'
+};
+
